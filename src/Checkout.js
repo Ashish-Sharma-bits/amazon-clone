7ed61eba750c8958019f7f03bd4074fb67b61740
Ashish-Sharma-bits <ashishlawaniyaa@gmail.com>
2021-11-05 11:44:50 +0530

    adding basket items

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,7 +1,12 @@
 import React from "react";
 import "./Checkout.css";
 import Subtotal from "./Subtotal";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="checkout">
       <div className="checkout_left">
@@ -13,7 +18,14 @@ function Checkout() {
         <div>
           <h2 className="checkout_title"> your basket </h2>
         </div>
-           
+        {basket.map((item) => (
+          <CheckoutProduct
+            title={item.title}
+            image={item.image}
+            price={item.price}
+            reating={item.rating}
+          />
+        ))}
       </div>
       <div className=" checkout_right">
         <div className="checkout_subtotal">
