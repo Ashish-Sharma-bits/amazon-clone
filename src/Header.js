cab9fd26b2b77677358df591993ffb85ed61b030
Ashish-Sharma-bits <ashishlawaniyaa@gmail.com>
2021-11-16 09:06:42 +0530

    user Email Display

--- src/Header.js
+++ src/Header.js
@@ -32,7 +32,7 @@ function Header() {
       <div className="header_nav">
         <Link to={!user && "/login"}>
           <div onClick={handleSignIn} className="header_option">
-            <span className="header_optionLineOne">hello guest</span>
+            <span className="header_optionLineOne">{user ? `Hi ${user.email}` : "Hello Guest!"}</span>
             <span className="header_optionLineTwo">
               {user ? "Sign Out" : "Sign In"}
             </span>
